1047293cd208306782100023af7d56389138b801
Gilang Heavy <38318210+gheav@users.noreply.github.com>
2022-02-17 16:41:36 +0700

    add Snap Redirect Method

---
+++ application/controllers/SnapRedirect.php
@@ -0,0 +1,60 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class SnapRedirect extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = '<YOUR SERVER SECRET KEY>';
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+    }
+    public function index()
+    {
+        $this->load->view('layouts/main', [
+            'orderID' => rand(),
+            'content' => 'midtrans/snap'
+        ]);
+    }
+
+    public function checkout()
+    {
+        $transaction = [
+            //Required
+            'transaction_details' => [
+                'order_id'      => $this->input->post('orderID'),
+                'gross_amount'  => $this->input->post('amount') // no decimal allowed for creditcard
+            ],
+            //Optional
+            'customer_details' => [
+                'first_name'    => $this->input->post('firstName'),
+                'last_name'     => $this->input->post('lastName'),
+                'email'         => $this->input->post('email'),
+                'phone'         => $this->input->post('phone'),
+            ],
+            //Optional
+            'item_details' => [
+                [
+                    'id'        => rand(1, 10),
+                    'price'     => $this->input->post('amount'),
+                    'quantity'  => 1,
+                    'name'      => $this->input->post('description'),
+                ]
+            ]
+        ];
+
+        try {
+            // Get Snap Payment Page URL
+            $paymentUrl = \Midtrans\Snap::createTransaction($transaction)->redirect_url;
+            // Redirect to Snap Payment Page
+            header('Location: ' . $paymentUrl);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
--- application/views/midtrans/snap.php
+++ application/views/midtrans/snap.php
@@ -1,54 +1,55 @@
 <h3>Transaction Page :</h3>
 <hr>
-
-<div class="row">
-    <div class="col-6">
-        <h5 class="fw-bold">Customer Detail</h5>
-        <div class="mb-3">
-            <div class="row g-3">
-                <div class="col-6">
-                    <label for="firstName" class="form-label">Firstname</label>
-                    <input type="text" class="form-control" id="firstName" name="firstName">
+<form action="<?= base_url('snapRedirect/checkout'); ?> " method="POST">
+    <div class="row">
+        <div class="col-6">
+            <h5 class="fw-bold">Customer Detail</h5>
+            <div class="mb-3">
+                <div class="row g-3">
+                    <div class="col-6">
+                        <label for="firstName" class="form-label">Firstname</label>
+                        <input type="text" class="form-control" id="firstName" name="firstName">
+                    </div>
+                    <div class="col-6">
+                        <label for="lastName" class="form-label">Lastname</label>
+                        <input type="text" class="form-control" id="lastName" name="lastName">
+                    </div>
                 </div>
-                <div class="col-6">
-                    <label for="lastName" class="form-label">Lastname</label>
-                    <input type="text" class="form-control" id="lastName" name="lastName">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone Number</label>
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="08xxxxxxx">
+            </div>
+
+        </div>
+        <div class="col-6">
+            <h5 class="fw-bold">Transaction Detail</h5>
+            <div class="mb-3">
+                <label for="orderID" class="form-label">Order ID :</label>
+                <input type="text" class="form-control" id="orderID" name="orderID" value="<?= $orderID; ?>" readonly>
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Transaction Description :</label>
+                <textarea class="form-control" id="description" name="description" rows="1"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="amount" class="form-label">Amount :</label>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Rp</span>
+                    <input type="number" min="0" class="form-control" id="amount" placeholder="0" name="amount" required>
                 </div>
             </div>
         </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
-        </div>
-        <div class="mb-3">
-            <label for="phone" class="form-label">Phone Number</label>
-            <input type="text" class="form-control" id="phone" name="phone" placeholder="08xxxxxxx">
-        </div>
-
     </div>
-    <div class="col-6">
-        <h5 class="fw-bold">Transaction Detail</h5>
-        <div class="mb-3">
-            <label for="orderID" class="form-label">Order ID :</label>
-            <input type="text" class="form-control" id="orderID" name="orderID" value="<?= $orderID; ?>" readonly>
-        </div>
-        <div class="mb-3">
-            <label for="description" class="form-label">Transaction Description :</label>
-            <textarea class="form-control" id="description" name="description" rows="1"></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="amount" class="form-label">Amount :</label>
-            <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">Rp</span>
-                <input type="number" min="0" class="form-control" id="amount" placeholder="0" name="amount" required>
-            </div>
-        </div>
+    <div class="text-end">
+        <button type="button" class="btn btn-primary" id="checkout">Snap Checkout</button>
+        <button type="submit" class="btn btn-info" id="checkout">Snap Redirect Checkout</button>
     </div>
-</div>
-<div class="text-end">
-    <button type="button" class="btn btn-primary" id="checkout">Snap Checkout</button>
-</div>
-
+</form>
 <hr>
 <div class=" ">
     <h3 class="fw-bold"> Midtrans Payment</h3>
